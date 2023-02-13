1bce22f258d0ce32b51b171bc7b840adecf475b0
elbqarey <medo@gmail.com>
2023-02-13 18:29:33 +0200

    upload file

---
+++ main.js
@@ -0,0 +1,21 @@
+function white (){
+  document.getElementById("lamp").style.color='white';
+}
+function red (){
+  document.getElementById("lamp").style.color='red';
+}
+function blue (){
+  document.getElementById("lamp").style.color='blue';
+}
+function yellow (){
+  document.getElementById("lamp").style.color='yellow';
+}
+function green (){
+  document.getElementById("lamp").style.color='green';
+}
+function orange (){
+  document.getElementById("lamp").style.color='orange';
+}
+function aqua (){
+  document.getElementById("lamp").style.color='aqua';
+}
